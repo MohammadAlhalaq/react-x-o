e1caeff8f27bcb2a389e618f9ce33de01ef84889
mohammad <mohammad@pop-os.localdomain>
2019-09-11 16:30:18 +0300

    add array and create x and o and create condition and winner

--- src/components/squer.js
+++ src/components/squer.js
@@ -1,38 +1,78 @@
 import React from 'react';
 import ChildButton from './childButton';
+import swal from 'sweetalert2';
 
-export default class Squer extends React.Component{
+export default class Squer extends React.Component {
     state = {
-        player :'X'
+        player: 'X',
+        result: Array(9).fill(null),
+        win: null,
     }
-    changePlayer = () => {
-        this.setState(prevState => ({ player: prevState.player === "X" ? "O": "X" }))
+    winner = () => {
+        const { result } = this.state;
+        const list = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for(let i=0 ;i<list.length;i++){
+            const [a, b, c] = list[i];
+            if (result[a] && result[a] === result[b] && result[b] === result[c]){
+                this.setState({ win: result[a]});
+                return ;
+            }
+        }  
+        return null;  
     }
-    squerRender(i){
-        return <ChildButton changePlayer={this.changePlayer} player={this.state.player} />
+    onClick = (i, cb) => {
+        const clonedArray = this.state.result.slice();
+        clonedArray[i] = this.state.player;
+        this.setState(prevState =>({player: prevState.player === "X" ? "O" : "X", result: clonedArray}), () => {
+            this.winner();
+        });
+    }
 
+    squerRender(i) {
+        return <ChildButton howwin={this.state.win}  onClick={(cb) => this.onClick(i, cb)} player={this.state.player} />
     }
-    render(){
-        return(
-            <div className='board'>
-                <h1>player: {this.state.player}</h1>
+
+    getWinner = () => {
+        swal.fire({
+            text: `Winner is ${this.state.win}`
+        })
+        return this.state.win
+    }
+
+    render() {
+        return (
+        <div>
+                {this.state.win?<h1 className="winner">hey winner: {this.getWinner()}</h1>:''}
+                <h1 className="winner">player: {this.state.player}</h1>
+            <div className="board">
                 <div className='board__line'>
+                    {this.squerRender(0)}
                     {this.squerRender(1)}
                     {this.squerRender(2)}
-                    {this.squerRender(3)}
                 </div>
                 <div className='board__line'>
+                    {this.squerRender(3)}
                     {this.squerRender(4)}
                     {this.squerRender(5)}
-                    {this.squerRender(6)}
                 </div>
                 <div className='board__line'>
+                    {this.squerRender(6)}
                     {this.squerRender(7)}
                     {this.squerRender(8)}
-                    {this.squerRender(9)}
                 </div>
+
                 {/* <button className="button" onClick={()=>this.setState({})}>play again</button> */}
-            </div> 
+            </div>
+        </div>
         );
     }
 }
